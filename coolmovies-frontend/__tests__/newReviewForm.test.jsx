fbdc25511fc18eb9472e5987b1f457bd8fdef789
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2024-01-30 19:11:00 -0300

    improving test quality for newReviewForm test

--- coolmovies-frontend/__tests__/newReviewForm.test.jsx
+++ coolmovies-frontend/__tests__/newReviewForm.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import { MockedProvider } from "@apollo/client/testing";
 import { act } from 'react-dom/test-utils';
 import { CREATE_MOVIE_REVIEW, CREATE_USER, NewReviewForm } from '../components/NewReviewForm'
+import { fireEvent } from '@testing-library/react';
 
 it("should render without error", () => {
 
@@ -78,31 +79,64 @@ it("should render without error", () => {
             <NewReviewForm message="70351289-8756-4101-bf9a-37fc8c7a82cd" />
           </MockedProvider>
         );
-            // Input name
-            userEvent.type(screen.getByLabelText('Name'), 'Test User')
+
+        //type TestElement = Document | Element | Window | Node
+
+function hasNameInputValue(e) {
+  return screen.getByDisplayValue('Test User') === e
+}
+
+function hasTitleInputValue(e) {
+    return screen.getByDisplayValue('Test Title') === e
+}
+
+function hasBodyInputValue(e) {
+    return screen.getByDisplayValue('Test Body') === e
+}
+
+
+            const nameInput = screen.getByLabelText('Name')
+            fireEvent.change(nameInput, { target: { name: "Name", value: "Test User" } })
+            //expect(hasInputValue(nameInput, 'Test User')).toBe(true)
+            //userEvent.type(screen.getByLabelText('Name'), 'Test User')
 
             // Input title
-            userEvent.type(screen.getByLabelText('Title *'), 'Test Title')
+            const titleInput = screen.getByLabelText('Title *')
+            fireEvent.change(titleInput, { target: { name: "Title *", value: "Test Title" } })
+            //userEvent.type(screen.getByLabelText('Title *'), 'Test Title')
 
             // Input rating
             userEvent.click(screen.getByPlaceholderText('Rate this movie:').querySelector('input[value="5"]'));
 
             // Input Body
-            userEvent.type(screen.getByLabelText('Write your review ... *'), 'Test Body')
+            const bodyInput = screen.getByLabelText('Write your review ... *')
+            fireEvent.change(bodyInput, { target: { name: "Write your review ... *", value: "Test Body" } })
+            //userEvent.type(screen.getByLabelText('Write your review ... *'), 'Test Body')
 
             // Submit form
             await act(async () => {
                 userEvent.click(screen.getByText('Submit'))
-                userEvent.click(screen.getByText('Submit'))
+                //userEvent.click(screen.getByText('Submit'))
             })
 
             // Wait for mutation and result
 
             await waitFor(() => {
-                expect(screen.getByText('Title *')).toBeInTheDocument()
-                expect(screen.getByText('Write your review ... *')).toBeInTheDocument()
+                //expect(hasInputValue(nameInput, 'Test User')).toBe(true)
+                //expect(screen.getByText('Write your review ... *')).toBeInTheDocument()
+                expect(hasNameInputValue(nameInput, 'Test User')).toBe(true)
+                expect(hasTitleInputValue(titleInput, 'Test Title')).toBe(true)
+                expect(hasBodyInputValue(bodyInput, 'Test Body')).toBe(true)
             })
         })
+
+        {/*
+    const input = screen.getByLabelText("Some Label")
+
+fireEvent.change(input, { target: { value: '123' } })
+expect(hasInputValue(input, "123")).toBe(true)
+    
+    */}
 
 
 // Developing the test for New Review Mutation
